1affd302d8d9fa24ca9dbd72d8ee0a90eac4f43b
NOTECOLT <d27thcolt@gmail.com>
2024-01-07 14:38:27 +0800

    Initial commit. added testfs

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+    CS140 2324A PROJECT 2 - EXT2 FILESYSTEM READER
+    Ivan Cassidy Cadiang (2021-12575)
+    Diego Montenejo (2020-05984)
+    Rohan Solas (2021-07292)
+*/
+
+// TODO:
+//  - Superblock Struct
+//  - Inode Struct
+//  - BGD Struct ?
+
+int main() {
+    return 0;   
+}
