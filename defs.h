e448917082a83e358c9adecdb9cb4fcbddf9e166
rbsolas <rbsolas@up.edu.ph>
2024-01-09 22:37:46 +0800

    feat: Added structs inode and directory_entry; Read root directory block

--- defs.h
+++ defs.h
@@ -1,7 +1,7 @@
 #include <linux/types.h>
 
 #define SUPERBLOCK_OFFSET 1024
-#define FD_DEV "/dev/loop20" // replace with the appropriate device
+#define FD_DEV "/dev/loop18" // replace with the appropriate device
 #define EXT2_MAGIC_NUMBER 0xEF53
 
 // Reference: https://wiki.osdev.org/Ext2 Base Superblock Fields
@@ -12,7 +12,7 @@ typedef struct superblock {
 	__u32 total_unallocated_blocks;
 	__u32 total_unallocated_inodes;
 	__u32 superblock_block_num;
-	__u32 lg_block_size; // (not actual block size) block size is computed as 1024 shifted to the left by block_size bits
+	__u32 lg_block_size; // (not actual block size) block size is computed as 1024 shifted to the left by lg_block_size bits
 	__u32 lg_fragment_size;
 	__u32 total_blocks_in_blockgroup;
 	__u32 total_fragments_in_blockgroup;
@@ -31,11 +31,13 @@ typedef struct superblock {
 	__u32 major_version;
 	__u16 user_id;
 	__u16 group_id;
-	__u8 unused[940];
+	__u32 first_nonreserved_inode;
+	__u16 inode_size;
+	__u8 unused[934];
 } superblock;
 
-// Block Group Descriptor Table
-typedef struct blk_groupdesc_tbl {
+// Block Group Descriptor
+typedef struct blk_groupdesc {
 	__u32 block_bitmap;
 	__u32 inode_bitmap;
 	__u32 inode_table;
@@ -43,4 +45,34 @@ typedef struct blk_groupdesc_tbl {
 	__u16 total_unallocated_inodes;
 	__u16 total_dirs;
 	__u8 unused[14];
-} blk_groupdesc_tbl;
+} blk_groupdesc;
+
+// Inodes
+typedef struct inode {
+	__u16 type_and_perm;
+	__u16 user_id;
+	__u32 lo_size;
+	__u32 last_access;
+	__u32 creation_time;
+	__u32 last_mod;
+	__u32 del_time;
+	__u16 group_id;
+	__u16 count_hard_links;
+	__u32 count_sectors;
+	__u32 flags;
+	__u32 ignored;
+	__u32 dp[12];
+	__u32 sip;
+	__u32 dip;
+	__u32 tip;
+	__u8 unused[28];
+} inode;
+
+// Directory Entry
+typedef struct dir_entry {
+	__u32 inode_num;
+	__u16 size;
+	__u8  name_size;
+	__u8  ignored;
+	__u8  name[256]; // MAY NEED TO CHANGE
+} dir_entry;
--- extract.c
+++ extract.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 #include "defs.h"
 
 int main() {
@@ -11,7 +12,7 @@ int main() {
 
     Boot Block (1024 bytes) -> Block Group 1 -> ... -> Block Group N
 
-    BG1 => Superblock (1024 bytes) -> Group Descriptors -> Data Block Bitmap -> Inode Bitmap
+    BG1 => Superblock (1024 bytes) -> BGDT -> Data Block Bitmap -> Inode Bitmap
             -> Inode Table -> Data Blocks
 
     Note: Use sudo when having permission issues
@@ -25,7 +26,8 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Seek to the superblock position (skip 1024 bytes)
+    // ===== Seek to the superblock position (skip 1024 bytes)
+    printf("-----SUPERBLOCK INFO-----\n");
     if (lseek(fd, SUPERBLOCK_OFFSET, SEEK_SET) == -1) {
         perror("Error: Seeking to superblock failed");
         close(fd);
@@ -48,37 +50,96 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Superblock Number: %d\n", sb.superblock_block_num);
+    printf("    Superblock Number: %d\n", sb.superblock_block_num);
 
     __u32 block_size = 1024 << sb.lg_block_size;
 
     __u32 partition_size = sb.total_blocks * block_size;
 
-    __u32 total_block_groups = partition_size / (8 * block_size);
-
-    printf("partition size: %d\n", partition_size);
-    printf("tbg: %d\n", total_block_groups);
+    __u32 total_block_groups = ceil(sb.total_blocks / sb.total_blocks_in_blockgroup + 1);
 
 
-    printf("Block Size: %d\n", block_size); // Block size
-    printf("Number of blocks per block group: %d\n", sb.total_blocks_in_blockgroup);
-    printf("Number of inodes per block group: %d\n", sb.total_inodes_in_blockgroup);
+    printf("    Partition size: %d\n", partition_size);
+    printf("    Total # of blocks: %d\n", sb.total_blocks);
+    printf("    Total # of block groups: %d\n", total_block_groups);
+    printf("    Total # of inodes: %d\n", sb.total_inodes);
 
-    // Seek to the Block Group Descriptor Table position (skip 4096 bytes = 1 block) 
-    blk_groupdesc_tbl bgd[total_block_groups];
+    printf("    Block size: %d\n", block_size);
+    printf("    # of blocks per block group: %d\n", sb.total_blocks_in_blockgroup); // FSR number is higher than total number of blocks
+    printf("    inode size: %d\n", sb.inode_size);
+    printf("    # of inodes per block group: %d\n", sb.total_inodes_in_blockgroup);
+    printf("    # of inode blocks per block group: %d\n", (sb.total_inodes_in_blockgroup / (block_size / sb.inode_size)));
 
-    if (lseek(fd, block_size, SEEK_SET) == -1) {
+    // ===== Seek to the Block Group Descriptor Table position (skip 4096 bytes = 1 block)
+    int bgdOffset = 0;
+    printf("-----BGD ENTRY %d INFO-----\n", bgdOffset);
+
+    blk_groupdesc bgdt[total_block_groups];
+
+    if (lseek(fd, block_size + (bgdOffset * 32), SEEK_SET) == -1) {
         perror("Error: Seeking to BGDT failed");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    read(fd, &bgd[0], sizeof(blk_groupdesc_tbl));
+    read(fd, &bgdt[bgdOffset], sizeof(blk_groupdesc));
 
-    printf("Block bitmap: %d\n", bgd[0].block_bitmap);
-    printf("inode bitmap: %d\n", bgd[0].inode_bitmap);
-    printf("inode table: %d\n", bgd[0].inode_table);
-    printf("unalloc blocks: %d\n", bgd[0].total_unallocated_blocks);
+    printf("    Block bitmap block address: %d\n", bgdt[bgdOffset].block_bitmap);
+    printf("    inode bitmap block address: %d\n", bgdt[bgdOffset].inode_bitmap);
+    printf("    inode table starting block address: %d\n", bgdt[bgdOffset].inode_table);
+    printf("    Unallocated blocks: %d\n", bgdt[bgdOffset].total_unallocated_blocks);
+    printf("    Total directories: %d\n", bgdt[bgdOffset].total_dirs);
+
+    // ===== Find an inode
+    int inodeNum = 2; // root
+    inode inode;
+    printf("-----INODE %d LOCATION-----\n", inodeNum);
+
+    int blockGroup = (inodeNum - 1) / sb.total_inodes_in_blockgroup;
+    int inodeIndex = (inodeNum - 1) % sb.total_inodes_in_blockgroup;
+    int containingBlock = (inodeIndex * sb.inode_size) / block_size;
+
+    printf("    Block group: %d\n", blockGroup);
+    printf("    Index: %d\n", inodeIndex);
+    printf("    Containing block: %d\n", containingBlock);
+
+    // ===== Seek to the inode table position (skip 4096 bytes * 4 = 4 blocks) 
+    printf("-----INODE %d INFO-----\n", inodeNum);
+
+    if (lseek(fd, block_size * 4 + (inodeIndex * sb.inode_size), SEEK_SET) == -1) {
+        perror("Error: Seeking to inode table failed");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    read(fd, &inode, sizeof(inode));
+
+    printf("    Type and permissions: %x\n", inode.type_and_perm);
+    printf("    File size (lo): %d\n", inode.lo_size);
+    for (int i = 1; i <= 12; i++){
+        printf("    Direct Pointer %d: %d\n", i, inode.dp[i-1]);
+    }
+
+    // ===== Read data block(s) pointed to by the direct pointer(s)
+    // NOTE: Code below only for one direct pointer (from root inode which only has one pointer)
+    printf("-----DATA BLOCK %d-----\n", inode.dp[0]);
+    dir_entry directory_entry;
+
+    int bytesParsed = 0;
+    while (bytesParsed < block_size) {
+        if (lseek(fd, block_size * inode.dp[0] + bytesParsed, SEEK_SET) == -1) {
+            perror("Error: Seeking to data block failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        read(fd, &directory_entry, sizeof(dir_entry));
+        printf("    inode number: %d\n", directory_entry.inode_num);
+        printf("    Directory entry size: %d\n", directory_entry.size);
+        printf("    Name size: %d\n", directory_entry.name_size);
+        printf("    Directory entry name: %s\n", directory_entry.name);
+
+        bytesParsed += directory_entry.size;
+    }
 
     close(fd);
 
