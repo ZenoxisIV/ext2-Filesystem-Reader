3aff60f7de4f9416761bd2e26e5e8330aa9393b1
Ivan <iacadiang@up.edu.ph>
2024-01-12 01:07:03 +0800

    experiment: added traversal for directories

--- extract.c
+++ extract.c
@@ -3,7 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <string.h>
 #include "defs.h"
+
+#define DIRECTORY 0x4000
+#define FILE 0x8000
+
+#define MAX_PATH_SIZE 4096
+
+inode readInode(int, int, superblock, int);
+void traverseAllPaths(inode, int, superblock, int, char**);
+
 
 int main() {
     /*
@@ -91,20 +101,41 @@ int main() {
     printf("    Total directories: %d\n", bgdt[bgdOffset].total_dirs);
 
     // ===== Find an inode
-    int inodeNum = 2; // root
-    inode inode;
-    printf("-----INODE %d LOCATION-----\n", inodeNum);
+    inode rootinode;
+    rootinode = readInode(2, fd, sb, block_size);
+    
+
+    //! WARNING: Beyond this point are experimental attempts.
+    // Issue: Can traverse but string manipulation for printing is wonky
+    // For now, traversal is limited to the first direct block
+    char* path = malloc(4096);
+    char* root = "/";
+    strncpy(path, root, 4096);
+
+    traverseAllPaths(rootinode, fd, sb, block_size, &path);
+
+    close(fd);
+
+    return 0;
+}
+
+
+inode readInode(int inodeNum, int fd, superblock sb, int block_size) {
+    // ===== Find an inode
+    //int inodeNum = 2; // root
+    inode currInode;
+    //printf("-----INODE %d LOCATION-----\n", inodeNum);
 
     int blockGroup = (inodeNum - 1) / sb.total_inodes_in_blockgroup;
     int inodeIndex = (inodeNum - 1) % sb.total_inodes_in_blockgroup;
     int containingBlock = (inodeIndex * sb.inode_size) / block_size;
 
-    printf("    Block group: %d\n", blockGroup);
-    printf("    Index: %d\n", inodeIndex);
-    printf("    Containing block: %d\n", containingBlock);
+    //printf("    Block group: %d\n", blockGroup);
+    //printf("    Index: %d\n", inodeIndex);
+    //printf("    Containing block: %d\n", containingBlock);
 
     // ===== Seek to the inode table position (skip 4096 bytes * 4 = 4 blocks) 
-    printf("-----INODE %d INFO-----\n", inodeNum);
+    //printf("-----INODE %d INFO-----\n", inodeNum);
 
     if (lseek(fd, block_size * 4 + (inodeIndex * sb.inode_size), SEEK_SET) == -1) {
         perror("Error: Seeking to inode table failed");
@@ -112,8 +143,9 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    read(fd, &inode, sizeof(inode));
-
+    read(fd, &currInode, sizeof(currInode));
+    return currInode;
+    /*
     printf("    Type and permissions: %x\n", inode.type_and_perm);
     printf("    File size (lo): %d\n", inode.lo_size);
     for (int i = 1; i <= 12; i++){
@@ -140,8 +172,64 @@ int main() {
 
         bytesParsed += directory_entry.size;
     }
-
-    close(fd);
-
-    return 0;
+    */
 }
+
+void traverseAllPaths(inode currInode, int fd, superblock sb, int block_size, char* path[]) {
+    printf("%s\n", *path);
+
+    dir_entry directory_entry;
+
+    int bytesParsed = 0;
+    while (bytesParsed < block_size) {
+
+        if (lseek(fd, block_size * currInode.dp[0] + bytesParsed, SEEK_SET) == -1) {
+            perror("Error: Seeking to data block failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        read(fd, &directory_entry, sizeof(dir_entry));
+
+        //printf("    inode number: %d\n", directory_entry.inode_num);
+        //printf("    Directory entry size: %d\n", directory_entry.size);
+        //printf("    Name size: %d\n", directory_entry.name_size);
+        //printf("    Directory entry name: %s\n", directory_entry.name);
+
+
+        if (strcmp(directory_entry.name, ".") == 0 || strcmp(directory_entry.name, "..") == 0) {
+            bytesParsed += directory_entry.size;
+            continue; // Skip current and parent directory entries
+        }
+
+        __u16 fileobject = currInode.type_and_perm & 0xF000; // extract type
+        //printf("%x\n", currInode.type_and_perm);
+        //printf("%x\n", fileobject);
+        sleep(1);
+
+        
+        switch (fileobject) {
+            case DIRECTORY:
+                inode nextInode = readInode(directory_entry.inode_num, fd, sb, block_size);
+                char* newPath = malloc(4096);
+                char* temp = directory_entry.name;
+                strncpy(newPath, *path, 4096);
+                strncat(newPath, temp, 4096);
+
+                //strcat(newPath, directory_entry.name);
+                traverseAllPaths(nextInode, fd, sb, block_size, &newPath);
+                strncat(newPath, "\0", 4096);
+                free(newPath);
+                newPath = NULL;
+
+                break;
+            case FILE:
+                break;
+            default:
+                printf("Warning: Unknown entity found");
+                break;
+        }
+
+        bytesParsed += directory_entry.size;
+    }
+}
+
